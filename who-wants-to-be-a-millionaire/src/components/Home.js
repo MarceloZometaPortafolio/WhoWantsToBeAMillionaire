6ecfe9b48bcd02cead8b048bda27ae979382c84f
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-01-16 20:15:48 -0700

    Basic layout finished

---
+++ who-wants-to-be-a-millionaire/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Home.css'
+
+const Home = () => {
+    return (
+        <div className="HomePage">
+            <h1>Who Wants To Be A Millionaire?</h1>
+            <p>
+                Hello!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ who-wants-to-be-a-millionaire/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/NavBar.css'
+
+const NavBar = () => {
+    return(
+        <div className="NavBar">
+            <ul>
+                <li>Game</li>
+                <li>Leader Board</li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
