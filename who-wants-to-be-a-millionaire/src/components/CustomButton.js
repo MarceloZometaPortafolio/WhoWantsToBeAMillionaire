6a1b40b15a174fff6b07a50d0fd987a3f4e54484
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-01-28 00:32:32 -0700

    custombutton can take a function from parent

--- who-wants-to-be-a-millionaire/src/components/CustomButton.js
+++ who-wants-to-be-a-millionaire/src/components/CustomButton.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../styles/CustomButtom.css'
 
-const CustomButton = (a) => {
+const CustomButton = (props) => {
+    const ButtonClicked = () => {
+        props.action();
+    }
+
     return(
-        <div>
-            <button>{a.message}</button>
+        <div className="custom-button">
+            <button onClick={ButtonClicked}>{props.message}</button>
         </div>
     );
 }
--- who-wants-to-be-a-millionaire/src/components/Home.js
+++ who-wants-to-be-a-millionaire/src/components/Home.js
@@ -3,10 +3,15 @@ import '../styles/Home.css'
 import CustomButton from './CustomButton'
 
 const Home = () => {
+
+    const ButtonAction = () => {
+        console.log("I was clicked!!!")
+    }
+
     return (
         <div className="HomePage">
             <h1>Who Wants To Be A Millionaire?</h1>
-            <CustomButton message={"Start new game"}/>
+            <CustomButton message={"Start new game"} action={ButtonAction}/>
         </div>
     );
 }
