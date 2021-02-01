16b628582f1b369ce7b28b6196043f18d4815c82
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-01-31 19:47:10 -0700

    Can display dialog box when clicked

--- who-wants-to-be-a-millionaire/src/components/Home.js
+++ who-wants-to-be-a-millionaire/src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../styles/Home.css'
 import CustomButton from './CustomButton'
-import { useHistory, withRouter } from 'react-router-dom';
+import { useHistory, withRouter, useState } from 'react-router-dom';
 
-const Home = () => {
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
+function Home () {
+    const [openDialog, setOpenDialog] = React.useState(false);
     const history = useHistory();
 
     const LeaderboardAction = () => {
@@ -12,7 +18,14 @@ const Home = () => {
     }
 
     const StartGameAction = () => {
+        setOpenDialog(true);
+
+
+    }
+
+    const handleCloseDialog = () => {
         console.log("I was clicked!")
+
     }
 
     return (
@@ -20,6 +33,23 @@ const Home = () => {
             <h1>Who Wants To Be A Millionaire?</h1>
             <CustomButton message={"Start new game"} action={StartGameAction}/>
             <CustomButton message={"Leader Board"} action={LeaderboardAction}/>
+
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+                <DialogTitle>Start new game!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        I am dialog box!
+                    </DialogContentText>
+                    <DialogActions>
+                        <button onClick={handleCloseDialog}>
+                            Cancel
+                        </button>
+                        <button onClick={handleCloseDialog}>
+                            Start
+                        </button>
+                    </DialogActions>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
