814226be963b8b27fb58a26f3101f88ad3acd9e7
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-02-17 14:46:47 -0700

    Can access individual elements of the JSON object

--- millionaire-api/DataService/dataService.js
+++ millionaire-api/DataService/dataService.js
@@ -1,8 +1,12 @@
 'use strict';
+
+const { response } = require("express");
 const fs = require("fs");
 module.exports.DataService = class DataService {
     constructor(){
         this.path = "\DataService\\questions.json";
+        this.jsonString = null;
+        this.jsonObject = null;
         console.log("Initiated new DataService");
     }
     
@@ -10,13 +14,26 @@ module.exports.DataService = class DataService {
         console.log("Hello from Data Service");
     }
 
-    readQuestions(){
-        var questions = fs.readFile(this.path, 'utf8', (err, jsonString) => {
+    async readQuestions(){
+        var questions = await fs.readFile(this.path, 'utf8', (err, jsonString) => {
             if(err){
                 console.log("File read failed: ", err);
                 return;
             }
-            console.log(jsonString);
-        })
+            
+            const questions = JSON.parse(jsonString);
+            const {game: [Level1, Level2]} = questions;
+            console.log(questions.game[0].Level1[0]);
+        });
+    }
+
+    async getQuestions(){
+        const questions = this.readQuestions();
+
+        // if(typeof myString !== 'undefined'){
+        //     const myObj = JSON.parse(myString);
+        //     console.log("Hit the getQuestions", myObj);
+        // }
+
     }
 }
