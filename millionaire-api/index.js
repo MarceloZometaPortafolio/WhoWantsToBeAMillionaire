ea53f480176492e8a2a50f805a0bf573fca84fe5
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-02-23 06:56:40 -0700

    api finally returns json successfully

--- millionaire-api/index.js
+++ millionaire-api/index.js
@@ -5,14 +5,34 @@ const express = require('express');
 const app = express();
 const dataService = require('./DataService/dataService')
 const port = 8000;
+const fs = require("fs");
+const { resolve } = require("path");
 
 app.get('/', async (req, res) => {
-  var myData = new dataService.DataService();
-  myData.greet();
-  
-  res.json({"message": "Hello world!"});
+  readQuestions()
+    .then(data => {
+      console.log("Returning data to the browser");
+      res.send(data);
+    })
+    .catch( error => res.status(500).send(error))
+    .finally(() => console.log("Done processing questions"));
 });
- 
+
+function readQuestions(){
+  const path = "\DataService\\questions.json";
+
+  return new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf8', (err, data) => {
+          if(err){
+              reject(err);
+          }
+          else{
+              let questions = JSON.parse(data);
+              resolve(data);
+          }
+  })     
+})}
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
