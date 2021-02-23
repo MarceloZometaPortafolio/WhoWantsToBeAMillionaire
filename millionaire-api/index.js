d0cb2e6bfff2d8f4328df57fd2c39b03f254ea35
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-02-23 07:12:22 -0700

    Cleaned the api

--- millionaire-api/index.js
+++ millionaire-api/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const { response } = require('express');
 const express = require('express');
 const app = express();
-const dataService = require('./DataService/dataService')
 const port = 8000;
 const fs = require("fs");
-const { resolve } = require("path");
+const path = "\Data\\questions.json";
 
 app.get('/', async (req, res) => {
   readQuestions()
@@ -19,8 +17,6 @@ app.get('/', async (req, res) => {
 });
 
 function readQuestions(){
-  const path = "\DataService\\questions.json";
-
   return new Promise((resolve, reject) => {
       fs.readFile(path, 'utf8', (err, data) => {
           if(err){
@@ -30,8 +26,8 @@ function readQuestions(){
               let questions = JSON.parse(data);
               resolve(data);
           }
-  })     
-})}
+  })});  
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
