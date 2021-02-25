b7c3ec7412dcc510e5f6884091b0aba4860a88cb
Marcelo Zometa <marcelo.zometa@hotmail.com>
2021-02-25 02:46:00 -0700

    Finally discovered bug. Question can now load

--- who-wants-to-be-a-millionaire/src/components/Game.js
+++ who-wants-to-be-a-millionaire/src/components/Game.js
@@ -3,22 +3,33 @@ import {useLocation} from "react-router-dom";
 import axios from 'axios';
 import readAllQuestions from '../data_service/DataService';
 import path from "../data_service/questions.json";
+import { Suspense } from 'react';
 
 function Game({props}) {
     const location = useLocation();
-    
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+
     const [showProgress, setShowProgress] = useState(true);    
-    const [questions, setQuestions] = useState("");
+    const [questions, setQuestions] = useState(null);
+
+    const loadQuestions = async() => {
+        console.log("Entering loadQuestions");
+        await sleep(1000);
+        const data = await readAllQuestions();
+        setQuestions(data);
+    }
 
     useEffect(() => {
         setShowProgress(true);
         //setQuestions(readAllQuestions());
-        readAllQuestions()
-            .then(data => {
-                setQuestions(data);
-                console.log("On game, questions are:", questions.game[0]);
-            })
-            .catch(err => console.log("Couldn't read the questions properly", err));
+        loadQuestions();
+            // .then(data => {
+            //     setQuestions(data);
+            //     console.log("On game, questions are:", questions.game[0]);
+            // })
+            // .catch(err => console.log("Couldn't read the questions properly", err));
             // .then(data => {
             //     console.log(data); 
             //     setQuestions(data);
@@ -31,6 +42,21 @@ function Game({props}) {
     return(
         <div className="Game">
             <h1>Question {location.state.question}</h1>
+            <h1>You have loaded {questions?.game[0].question}</h1>
+            
+            {/* if (isEmpty(questions) ) {
+                <h1>Loading....</h1>
+            }
+            else {
+            } */}
+
+            {/* <Suspense fallback={<p>Loading...</p>}>
+                if (questions == null) {
+                    <h1>Loading!</h1>
+                }
+                else{
+                }
+            </Suspense> */}
         </div>
     );
 }
--- who-wants-to-be-a-millionaire/src/data_service/DataService.js
+++ who-wants-to-be-a-millionaire/src/data_service/DataService.js
@@ -1,18 +1,23 @@
 import path from "./questions.json";
 
- function readAllQuestions(){
-    try{
-        return new Promise((resolve, reject) => {
-          const jsonString = JSON.stringify(path);
-          const questions = JSON.parse(jsonString);
+ async function readAllQuestions(){
+    // try{
+    //     return new Promise((resolve, reject) => {
+    //       const jsonString = JSON.stringify(path);
+    //       const questions = JSON.parse(jsonString);
   
-          console.log("These are the questions", questions);
-          return resolve(questions);              
-        });
-    }
-    catch(err){
-        console.log("Error found", err);
-    }
+    //       console.log("These are the questions", questions);
+    //       return resolve(questions);              
+    //     });
+    // }
+    // catch(err){
+    //     console.log("Error found", err);
+    // }
+      const jsonString = JSON.stringify(path);
+      const questions = JSON.parse(jsonString);
+
+      console.log("These are the questions", questions);
+      return questions;
   }
 
 export default readAllQuestions;
